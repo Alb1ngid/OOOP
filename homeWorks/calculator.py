420c7b687cc10c678dee1cdc2f78206af16b3f28
maxcrimea <maxcrimea50@gmail.com>
2023-01-11 23:37:53 +0600

    'hw3'

---
+++ homeWorks/calculator.py
@@ -0,0 +1,45 @@
+from homeWorks.hw3 import Bank
+
+
+def __add__(self, other):
+    if isinstance(other, Bank):
+        self._balance = self._balance + other._balance
+    if isinstance(other, (int, float)):
+        self._balance = self._balance + other
+
+
+def __radd__(self, other):
+    return self + other
+
+
+def __mul__(self, other):
+    if isinstance(other, Bank):
+        self._balance = self._balance * other._balance
+    if isinstance(other, (int, float)):
+        self._balance = self._balance * other
+
+
+def __rmul__(self, other):
+    return self * other
+
+
+def __sub__(self, other):
+    if isinstance(other, Bank):
+        self._balance = self._balance - other._balance
+    if isinstance(other, (int, float)):
+        self._balance = self._balance - other
+
+
+def __rsub__(self, other):
+    return self - other
+
+
+def __truediv__(self, other):
+    if isinstance(other, Bank):
+        self._balance = self._balance / other._balance
+    if isinstance(other, (int, float)):
+        self._balance = self._balance / other
+
+
+def __rtruediv__(self, other):
+    return self / other
--- homeWorks/hw3.py
+++ homeWorks/hw3.py
@@ -1,1 +1,31 @@
-print('работать тут')
+import calculator
+class Bank:
+    def __init__(self, name, balance):
+        self._name = name
+        self._balance = balance
+
+    def moneyX(self, plus_balance=int(input('Введите сумму которую хотите добавить на свой счёт: '))):
+        self._balance = plus_balance + self._balance
+        return f'Вы внесли {plus_balance}, теперь баланс вашего счёта составляет {self._balance}'
+
+    def _kill(self, minus_balace=int((input('Введите сумму которую хотите снять с вашего счёта: ')))):
+        self._balance = self._balance - minus_balace
+        return f'Вы сняли {minus_balace}, теперь баланс вашего счёта составляет {self._balance}'
+
+    def __jackpot(self):
+        self._balance = self._balance * 10
+        return f'Поздравляем! Вы сорвали джекпот, теперь баланс вашего счёта состввляет {self._balance}'
+
+    def _copy_balance(self, copy):
+        self._balance = copy._balance + self._balance
+        return f'Вы скопировали баланс у {copy._name} теперь баланс вашего счёта составляет: {self._balance}'
+
+
+test = Bank('Max', 100)
+print(test._name)
+print(test.moneyX())
+print(test._kill())
+print(test._Bank__jackpot())
+test2 = Bank('Vika', 200)
+print(test2._copy_balance(test))
+
